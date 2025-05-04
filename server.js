8a98156115ad2d654d8ed6d9882ced54c57d22e0
woadudakand <woadud.sovware@gmail.com>
2025-05-04 22:54:35 +0600

    update member id

--- server.js
+++ server.js
@@ -36,30 +36,36 @@ io.on('connection', (socket) => {
 
         socket.to(roomName).emit('user-joined', socket.id);
 
-        socket.on('send-offer', ({ to, offer }) => {
+        socket.on('send-offer', ({ to, offer, memberId }) => {
             io.to(to).emit('receive-offer', { from: socket.id, offer });
         });
 
         socket.on('send-answer', ({ to, answer }) => {
-            io.to(to).emit('receive-answer', { from: socket.id, answer });
+            io.to(to).emit('receive-answer', {
+                from: socket.id,
+                answer,
+                memberId,
+            });
         });
 
         socket.on('send-ice', ({ to, candidate }) => {
             io.to(to).emit('receive-ice', { from: socket.id, candidate });
         });
 
+        socket.on('raise-hand', ({ room }) => {
+            socket.to(room).emit('user-raised-hand', socket.id);
+        });
+
+        socket.on('send-chat', ({ room, message }) => {
+            socket
+                .to(room)
+                .emit('receive-chat', { senderId: socket.id, message });
+        });
+
         socket.on('disconnect', () => {
             rooms[roomName] = rooms[roomName].filter((id) => id !== socket.id);
             socket.to(roomName).emit('user-left', socket.id);
         });
-    });
-
-    socket.on('raise-hand', ({ room }) => {
-        socket.to(room).emit('user-raised-hand', socket.id);
-    });
-
-    socket.on('send-chat', ({ room, message }) => {
-        socket.to(room).emit('receive-chat', { senderId: socket.id, message });
     });
 });
 
